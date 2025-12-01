af881edd412fb19b32c19b872cb9da7d38c2cff4
Shota Silagadze <shotasilagadze@shotas-MacBook-Pro.local>
2025-12-01 13:00:26 +0400

    fix comment

--- internal/xconsensus/instanceproto/interfaces.go
+++ internal/xconsensus/instanceproto/interfaces.go
@@ -87,7 +87,8 @@ func (s *InstanceSequencer) Decide(instance compose.InstanceID, decision bool) e
 	/*
 		Instance could have been decided "false" when we voted - false so the instance was deleted already.
 		In this case decision "false" can arrive twice, once from current sequencer and once from SP.
-		If decision is true and we can't find the instance, it's an error.
+		If decision is true and we can't find the instance, it's an error. Becasuse instance cannot decide
+		"true" by itself and it should be received from SP only once.
 	*/
 	if !ok && decision {
 		return fmt.Errorf("could not find sequencer instance by %s", hex.EncodeToString(instance[:]))
