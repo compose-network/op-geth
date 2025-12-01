c6a41ebaaf1cd3d23cad6960645fc57898d89d08
Shota Silagadze <shotasilagadze@shotas-MacBook-Pro.local>
2025-12-01 12:44:29 +0400

    remove messanger

--- internal/xbootstrap/prover.go
+++ internal/xbootstrap/prover.go
@@ -9,18 +9,18 @@ import (
 
 type SimpleProver struct{}
 
-func NewSimpleProver() sbcp.SequencerProver {
+func NewSimpleProver() sbcp.Prover {
 	return &SimpleProver{}
 }
 
-func (p *SimpleProver) RequestProofs(blockHeader *sbcp.BlockHeader, superblockNumber compose.SuperblockNumber) []byte {
+func (p *SimpleProver) RequestProofs(blockHeader *sbcp.BlockHeader, superblockNumber compose.SuperblockNumber) {
 	if blockHeader == nil {
 		fmt.Printf("No sealed block for superblock #%d\n", superblockNumber)
-		return nil
+		return
 	}
 
 	fmt.Printf("Unimplemented: Requesting proofs for block #%d (hash: %s) at superblock #%d\n",
 		blockHeader.Number, blockHeader.BlockHash, superblockNumber)
 
-	return nil
+	return
 }
