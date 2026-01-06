df7cd57b8cf470c482cc0c710e47b76eb0f9b8f8
Shota Silagadze <shota@ssvlabs.io>
2026-01-06 21:09:17 +0400

    fix smoke tests

--- eth/api_backend_simulation_basic_test.go
+++ eth/api_backend_simulation_basic_test.go
@@ -303,9 +303,7 @@ func TestApplyPutInboxMessage(t *testing.T) {
 	blockCtx := core.NewEVMBlockContext(header, chainCtx, nil, backend.ChainConfig(), stateCopy)
 	vmCfg := vm.Config{}
 
-	gasPool := new(core.GasPool).AddGas(header.GasLimit)
-	usedGas := uint64(0)
-	if _, _, _, err := backend.applyPutInboxMessage(blockCtx, vmCfg, stateCopy, header, msg, gasPool, &usedGas, 0); err != nil {
+	if err := backend.applyPutInboxMessage(blockCtx, vmCfg, stateCopy, msg); err != nil {
 		t.Fatalf("applyPutInboxMessage returned error: %v", err)
 	}
 	// Confirm nonce has been updated
